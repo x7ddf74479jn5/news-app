f8bfb2f3cad7e471247de7b79e4867847bdd82e0
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2022-12-11 20:41:56 +0900

    :tada: init: environment setup

---
+++ app/head.tsx
@@ -0,0 +1,9 @@
+export default function Head() {
+  return (
+    <>
+      <title>Pandashark News</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+    </>
+  );
+}
