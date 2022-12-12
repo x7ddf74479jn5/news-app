3e12ae59a1877306d88e04e1e83cca0b1f5a6eaa
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2022-12-12 16:27:09 +0900

    :sparkles: feat: SearchBox

--- app/Header.tsx
+++ app/Header.tsx
@@ -1,6 +1,7 @@
 import { Bars3Icon } from "@heroicons/react/24/solid";
 import Link from "next/link";
 import NavLinks from "./NavLinks";
+import SearchBox from "./SearchBox";
 
 const Header = () => {
   return (
@@ -21,6 +22,8 @@ const Header = () => {
       </div>
 
       <NavLinks />
+
+      <SearchBox />
     </header>
   );
 };
