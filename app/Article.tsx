00cc1a09353a8b3275c258899962a11553aeef3f
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2022-12-12 20:42:10 +0900

    :lipstick: style: Article

--- app/Article.tsx
+++ app/Article.tsx
@@ -6,7 +6,7 @@ type Props = {
 };
 const Article = ({ article }: Props) => {
   return (
-    <article className="bg-slate-100 dark:bg-slate-800 flex flex-col rounded-lg shadow-sm hover:scale-105 hover:shadow-lg hover:bg-slate-200 transition-all duration-200 ease-out">
+    <article className="bg-slate-100 dark:bg-slate-800 flex flex-col rounded-lg shadow-lg hover:scale-105 hover:shadow-xl hover:bg-slate-200 transition-all duration-200 ease-out">
       {article.image && (
         <img src={article.image} alt={article.title} className="h-56 w-full object-cover rounded-t-lg shadow-md" />
       )}
